b0291daeef200e543d3ca1784f33274c59249dff
Elias Holm <eliasholm8@gmail.com>
2026-01-29 23:59:47 +0100

    Updated Sentry org

--- next.config.ts
+++ next.config.ts
@@ -9,9 +9,9 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  org: "elias-ht",
+  org: "iv1201-group7",
 
-  project: "javascript-nextjs",
+  project: "recruitment-app",
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
