35a945b0bcacee677a508a35354b0fa613168552
Simon Carlsson <operator_ax.09@icloud.com>
2026-01-30 11:23:09 +0100

    Fix linting errors

--- lib/database.ts
+++ lib/database.ts
@@ -1,20 +1,20 @@
-const { Pool } = require("pg");
+import { Pool } from "pg";
 
 const pool = new Pool({
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	database: process.env.DB_NAME,
-	password: process.env.DB_PASSWORD,
-	port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 });
 
 /**
  * Function to perform a query on the database.
- * 
+ *
  * @param text The SQL query text.
  * @param params Optional parameters for the SQL query.
  * @returns SQL query result.
  */
-export async function query(text: string, params?: any[]) {
-	return await pool.query(text, params);
+export async function query(text: string, params?: unknown[]) {
+  return await pool.query(text, params);
 }
