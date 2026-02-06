7064c973af63010b032b7de3b0cd7b10345f4275
Simon Carlsson <operator_ax.09@icloud.com>
2026-02-06 17:59:19 +0100

    Add styling ApplicationCard

--- app/[locale]/admin/ApplicationCard.tsx
+++ app/[locale]/admin/ApplicationCard.tsx
@@ -9,18 +9,16 @@ interface ApplicationCardProps {
 /**
  * ApplicationCard component displays individual application details.
  *
- * @param {ApplicationCardProps} application The properties for the application card, including applicant's name and application date.
+ * @param {ApplicationCardProps} application The properties for the application card.
  * @returns {JSX.Element} The rendered application card component.
  */
 const ApplicationCard = ({ application }: ApplicationCardProps) => {
   const t = useTranslations("AdminPage.applicationCard");
   const { name, applicationDate } = application;
   return (
-    <Card className="cursor-pointer hover:shadow-md transition-shadow">
-      <CardHeader className="pb-3">
-        <CardTitle className="text-base font-medium">{name}</CardTitle>
-      </CardHeader>
+    <Card className="cursor-pointer hover:shadow-md transition-shadow py-3">
       <CardContent>
+        <CardTitle className="text-base font-medium pb-1">{name}</CardTitle>
         <p className="text-sm text-muted-foreground">
           {t("appliedDate")}: {applicationDate}
         </p>
