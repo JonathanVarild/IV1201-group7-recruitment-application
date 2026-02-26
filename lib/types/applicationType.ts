8644dda226fecfbbc0dc4de7c295b5bd4598847e
Elias Holm <eliholm@kth.se>
2026-02-26 18:22:34 +0100

    Revert "Removed unused type"
    This reverts commit d7a65a19adc7bddbf408dc9d4870dd47fad9c100.

--- lib/types/applicationType.ts
+++ lib/types/applicationType.ts
@@ -1,3 +1,19 @@
+export interface ApplicationFullInformation {
+  id: string;
+  name: {
+    firstName: string;
+    lastName: string;
+  };
+  username: string;
+  email: string;
+  applicationDate: string;
+  status: string;
+  answers: {
+    question: string;
+    answer: string;
+  }[];
+}
+
 export interface SubmittedApplication {
   status: string;
   createdAt: Date;
