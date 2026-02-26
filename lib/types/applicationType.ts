d7a65a19adc7bddbf408dc9d4870dd47fad9c100
Elias Holm <eliholm@kth.se>
2026-02-26 17:48:51 +0100

    Removed unused type

--- lib/types/applicationType.ts
+++ lib/types/applicationType.ts
@@ -1,19 +1,3 @@
-export interface ApplicationFullInformation {
-  id: string;
-  name: {
-    firstName: string;
-    lastName: string;
-  };
-  username: string;
-  email: string;
-  applicationDate: string;
-  status: string;
-  answers: {
-    question: string;
-    answer: string;
-  }[];
-}
-
 export interface SubmittedApplication {
   status: string;
   createdAt: Date;
